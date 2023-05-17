5d75fe4410bac47a2b6cf8fd5390ff855309005f
Ahmet Özaydın <ahmetozaydn944@gmail.com>
2023-05-17 23:08:05 +0300

    layout integration: bottom navigation

--- app/src/main/java/com/kernelsoft/quora_clone/MainActivity.kt
+++ app/src/main/java/com/kernelsoft/quora_clone/MainActivity.kt
@@ -1,16 +1,47 @@
 package com.kernelsoft.quora_clone
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.kernelsoft.quora_clone.presentation.AddQuestion
+import com.kernelsoft.quora_clone.ui.theme.Gray100
+import com.kernelsoft.quora_clone.ui.theme.Gray50
 import com.kernelsoft.quora_clone.ui.theme.QuoracloneTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +53,361 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     QuoracloneTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreen() {
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = { AppBar("Home") },
+        bottomBar = { BottomBar(navController = navController) }
+    ) {
+        BottomNavGraph(navController = navController)
+    }
+}
+@OptIn(ExperimentalMaterialApi::class)
+@SuppressLint("UnrememberedMutableState")
+@Composable
+fun AppBar(
+    title: String,
+    actions: @Composable RowScope.() -> Unit = {}
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val sheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        animationSpec  = SwipeableDefaults.AnimationSpec,
+        confirmStateChange =  {
+            it != ModalBottomSheetValue.HalfExpanded }
+    )
+    var showBottomSheet by remember { mutableStateOf(false) }
+    var isClicked by mutableStateOf(false)
+    val appBarHorizontalPadding = 4.dp
+    val titleIconModifier = Modifier.fillMaxHeight()
+        .fillMaxWidth() // width(1230.dp - appBarHorizontalPadding)
+
+    Column {
+        TopAppBar(
+            backgroundColor = Gray50,
+            elevation = 0.dp,
+            modifier = Modifier.fillMaxWidth(),
+
+        ){
+                Box(Modifier.height(32.dp)) {
+                    Row(Modifier.fillMaxSize(),
+                        verticalAlignment = Alignment.CenterVertically) {
+                        ProvideTextStyle(value = MaterialTheme.typography.h2) {
+                            CompositionLocalProvider(
+                                LocalContentAlpha provides ContentAlpha.high,
+                            ){
+                                Text(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    textAlign = TextAlign.Center,
+                                    fontWeight = FontWeight.Bold,
+                                    style = TextStyle(fontSize = 18.sp),
+                                    maxLines = 1,
+                                    text = title,
+                                )
+                            }
+                        }
+                    }
+
+                    //actions
+                    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                        Row(
+                            Modifier.fillMaxHeight(),
+                            horizontalArrangement = Arrangement.End,
+                            verticalAlignment = Alignment.CenterVertically,
+                            content = actions
+                        )
+                    }
+                    Row(titleIconModifier, verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End) {
+                        CompositionLocalProvider(
+                            LocalContentAlpha provides ContentAlpha.high,
+                        ) {
+                            IconButton(
+                                onClick = { isClicked = true
+                                    coroutineScope.launch {
+                                        sheetState.show()
+                                    }
+                                          },
+                                enabled = true,
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.vc_add),
+                                    contentDescription = "Add Question",
+                                )
+                            }
+                        }
+                    }
+
+                }
+        }
+        Spacer(modifier = Modifier.fillMaxWidth().height(1.dp).background(color = Gray100))
+    }
+    BackHandler(sheetState.isVisible) {
+        coroutineScope.launch { sheetState.hide() }
+    }
+    /*if(isClicked){
+        LaunchedEffect(key1 = Unit){
+            coroutineScope.launch {
+                sheetState.show()
+            }
+
+        }
+
+       // BottomSheetLayout(isClicked,sheetState)
+        isClicked = false
+    }*/
+    /*Button(onClick = { isClicked = !isClicked }, modifier = Modifier.fillMaxWidth()){
+        Text("click me to open bottomsheet!")
+    }*/
+    /*LaunchedEffect(key1 = Unit){
+        coroutineScope.launch {
+            if (sheetState.isVisible) sheetState.hide()
+            else sheetState.show()
+        }
+    }*/
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetContent = { BottomSheet() },
+        modifier = Modifier.fillMaxWidth().fillMaxHeight()
+    ) { }
+}
+
+@Composable
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Home,
+        BottomBarScreen.Profile,
+        BottomBarScreen.Settings,
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    BottomNavigation {
+        screens.forEach { screen ->
+            AddItem(
+                screen = screen,
+                currentDestination = currentDestination,
+                navController = navController
+            )
+        }
+    }
+}
+
+@Composable
+fun RowScope.AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    BottomNavigationItem(
+        label = {
+            Text(text = screen.title)
+        },
+        icon = {
+            Icon(
+                imageVector = screen.icon,
+                contentDescription = "Navigation Icon"
+            )
+        },
+        selected = currentDestination?.hierarchy?.any {
+            it.route == screen.route
+        } == true,
+        unselectedContentColor = LocalContentColor.current.copy(alpha = ContentAlpha.disabled),
+        onClick = {
+            navController.navigate(screen.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        }
+    )
+}
+
+
+@Composable
+fun BottomNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = BottomBarScreen.Home.route
+    ) {
+        composable(route = BottomBarScreen.Home.route) {
+            HomeScreen()
+        }
+        composable(route = BottomBarScreen.Profile.route) {
+            ProfileScreen()
+        }
+        composable(route = BottomBarScreen.Settings.route) {
+            SettingsScreen()
+        }
+    }
+}
+sealed class BottomBarScreen(
+    val route: String,
+    val title: String,
+    val icon: ImageVector
+) {
+    object Home : BottomBarScreen(
+        route = "home",
+        title = "Home",
+        icon = Icons.Default.Home
+    )
+
+    object Profile : BottomBarScreen(
+        route = "profile",
+        title = "Profile",
+        icon = Icons.Default.Person
+    )
+
+    object Settings : BottomBarScreen(
+        route = "settings",
+        title = "Settings",
+        icon = Icons.Default.Settings
+    )
+}
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "HOME",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+@Preview
+fun HomeScreenPreview() {
+    HomeScreen()
+}
+@Composable
+fun ProfileScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "PROFILE",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "SETTINGS",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+fun BottomSheetLayout(isOpen : Boolean, sheetState: ModalBottomSheetState){
+
+
+    val coroutineScope = rememberCoroutineScope()
+
+    BackHandler(sheetState.isVisible) {
+        coroutineScope.launch { sheetState.hide() }
+    }
+    LaunchedEffect(key1 = Unit){
+        coroutineScope.launch {
+            if (sheetState.isVisible) sheetState.hide()
+            else sheetState.show()
+        }
+    }
+
+
+    ModalBottomSheetLayout(
+        sheetState = sheetState,
+        sheetContent = { BottomSheet() },
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        /*Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 24.dp)
+                .padding(horizontal = 24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Welcome to bottom sheet playground!",
+                modifier = Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.h4,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        if (sheetState.isVisible) sheetState.hide()
+                        else sheetState.show()
+                    }
+                }
+            ) {
+                Text(text = "Click to show bottom sheet")
+            }
+        }*/
+    }
+}
+
+@Composable
+fun BottomSheet() {
+    Column(
+        modifier = Modifier.padding(32.dp).fillMaxWidth().fillMaxHeight(0.95f)
+    ) {
+        Text(
+            text = "Bottom sheet",
+            style = MaterialTheme.typography.h6
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(
+            text = "Click outside the bottom sheet to hide it",
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/kernelsoft/quora_clone/presentation/AddQuestion.kt
@@ -0,0 +1,8 @@
+package com.kernelsoft.quora_clone.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddQuestion(){
+
+}
